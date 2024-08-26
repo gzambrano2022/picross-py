b58067950f041043f86b8fb8e707f61d420004f7
Benjamin <benjamin.villarroel3@gmail.com>
2024-08-26 12:18:40 -0400

    feat: Método en clase board drawPuzzle() para 10x10 y 5x5 (tentativo)

--- board.py
+++ board.py
@@ -6,9 +6,21 @@ class Board:
         self.figure = figure
 
     def drawPuzzle(self, window):
-        window.fill('white')
-        space = 50
-        for i in range(self.size):
-            pygame.draw.rect(window, 'black', (space, 10, 5, 500))
-            pygame.draw.rect(window, 'black', (30, space, 500, 5))
-            space += 50
+
+        if(self.size != 10 and self.size != 5): # disposiciones de tablero admitidas por ahora
+            return
+
+        elif(self.size == 10):
+            window.fill('white')
+            space = 50
+            for i in range(self.size+1):
+                pygame.draw.rect(window, 'black', (space, 10, 5, 540))
+                pygame.draw.rect(window, 'black', (50, space, 540, 5))
+                space += 50
+        elif(self.size == 5+1):
+            window.fill('white')
+            space = 60
+            for i in range(self.size):
+                pygame.draw.rect(window, 'black', (space, 10, 5, 500))
+                pygame.draw.rect(window, 'black', (30, space, 500, 5))
+                space += 60
