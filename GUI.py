cf741f0c87261ddcc01fa80fe3e04e3b546195ce
Benjamin <benjamin.villarroel3@gmail.com>
2024-09-30 21:11:14 -0300

    feat: grid centrado exitosamente junto a handle_click()

--- GUI.py
+++ GUI.py
@@ -3,12 +3,12 @@ from enum import Enum
 
 class SettingsManager(Enum):
     GRID_SIZE = 10
-    CELL_SIZE = 30
+    CELL_SIZE = 50
     WIDTH = 800
     HEIGHT = 600
     DEFAULT_COLOR = (255, 255, 255)
     CLICKED_COLOR = (0, 0, 0)
-    BACKGROUND_COLOR = 'brown'
+    BACKGROUND_COLOR = 'gray'
 
 class Cell:
     def __init__(self):
@@ -26,17 +26,25 @@ class Board:
         self.grid_size = grid_size
         self.figure = figure
         self.board = [[Cell() for _ in range(grid_size)] for _ in range(grid_size)]
+        self.offset_x = (SettingsManager.WIDTH.value - self.grid_size * self.cell_size) // 2
+        self.offset_y = (SettingsManager.HEIGHT.value - self.grid_size * self.cell_size) // 2
 
     def draw(self, surface):
         for row, rowOfCells in enumerate(self.board):
             for col, cell in enumerate(rowOfCells):
                 color = cell.get_color()
-                pygame.draw.rect(surface, color, (col * self.cell_size + 1, row * self.cell_size + 1, self.cell_size - 2, self.cell_size - 2))
+                pygame.draw.rect(surface, color, (
+                    self.offset_x + col * self.cell_size,  # Coordenada x ajustada
+                    self.offset_y + row * self.cell_size,  # Coordenada y ajustada
+                    self.cell_size - 2, self.cell_size - 2))  # Tamaño de la celda con un borde pequeño
+                # print(self.offset_x + col * self.cell_size)
+                # print(self.offset_y + row * self.cell_size)
+                # print(self.cell_size - 2, self.cell_size - 2)
                 
     def handle_click(self, pos): # pos son coordenadas (x,y) en pygame
-        row = pos[1] // self.cell_size
+        row = (pos[1]-self.offset_y) // self.cell_size
         print(f"pos[1] // self.cell_size = {row}")
-        col = pos[0] // self.cell_size
+        col = (pos[0]-self.offset_x) // self.cell_size
         print(f"pos[0] // self.cell_size = {col}")
         if 0 <= row < self.grid_size and 0 <= col < self.grid_size:
             self.board[row][col].click()
