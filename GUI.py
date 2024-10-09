bca8f5bbb0135e9ff610ff8d6bfd9ac668dc05eb
Benjamin <benjamin.villarroel3@gmail.com>
2024-10-09 20:55:32 -0300

    feat: mark() in Cell added, getColor() changes, now right click mark cells red

--- GUI.py
+++ GUI.py
@@ -4,21 +4,28 @@ from enum import Enum
 class SettingsManager(Enum):
     GRID_SIZE = 10
     CELL_SIZE = 50
-    WIDTH = 800
-    HEIGHT = 600
+    WIDTH = 1280
+    HEIGHT = 720
     DEFAULT_COLOR = (255, 255, 255)
     CLICKED_COLOR = (0, 0, 0)
+    MARKED_COLOR = (255, 0, 0)
     BACKGROUND_COLOR = 'gray'
 
 class Cell:
     def __init__(self):
         self.clicked = False
-
+        self.marked = False
     def click(self):
         self.clicked = not self.clicked
-
+    def mark(self):
+        self.marked = not self.marked
     def get_color(self):
-        return SettingsManager.CLICKED_COLOR.value if self.clicked else SettingsManager.DEFAULT_COLOR.value
+        if self.clicked:
+            return SettingsManager.CLICKED_COLOR.value
+        elif self.marked:
+            return SettingsManager.MARKED_COLOR.value
+        else:
+            return SettingsManager.DEFAULT_COLOR.value
 
 class Board:
     def __init__(self, cell_size, grid_size, figure):
@@ -37,23 +44,23 @@ class Board:
                     self.offset_x + col * self.cell_size,  # Coordenada x ajustada
                     self.offset_y + row * self.cell_size,  # Coordenada y ajustada
                     self.cell_size - 2, self.cell_size - 2))  # Tamaño de la celda con un borde pequeño
-                # print(self.offset_x + col * self.cell_size)
-                # print(self.offset_y + row * self.cell_size)
-                # print(self.cell_size - 2, self.cell_size - 2)
                 
-    def handle_click(self, pos): # pos son coordenadas (x,y) en pygame
-        row = (pos[1]-self.offset_y) // self.cell_size
-        print(f"pos[1] // self.cell_size = {row}")
-        col = (pos[0]-self.offset_x) // self.cell_size
-        print(f"pos[0] // self.cell_size = {col}")
-        if 0 <= row < self.grid_size and 0 <= col < self.grid_size:
-            self.board[row][col].click()
+    def handle_click(self, pos, num_click): # pos son coordenadas (x,y) en pygame. num_click: 1 right, 2 left
+        if num_click == 1:
+            row = (pos[1]-self.offset_y) // self.cell_size
+            col = (pos[0]-self.offset_x) // self.cell_size
+            if 0 <= row < self.grid_size and 0 <= col < self.grid_size:
+                self.board[row][col].click()
+        elif num_click == 2:
+            row = (pos[1]-self.offset_y) // self.cell_size
+            col = (pos[0]-self.offset_x) // self.cell_size
+            if 0 <= row < self.grid_size and 0 <= col < self.grid_size:
+                self.board[row][col].mark()
 
 
 class Game:
     def __init__(self, grid_size=SettingsManager.GRID_SIZE.value, cell_size=SettingsManager.CELL_SIZE.value):
         pygame.init()
-        # self.window_size = grid_size * cell_size
         self.window = pygame.display.set_mode((SettingsManager.WIDTH.value, SettingsManager.HEIGHT.value))
         self.clock = pygame.time.Clock()
         self.board = Board(cell_size, grid_size, "hola")
@@ -63,9 +70,11 @@ class Game:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
-            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                self.board.handle_click(event.pos)
-                print(event.pos)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    self.board.handle_click(event.pos,1)
+                elif event.button == 3:
+                    self.board.handle_click(event.pos, 2)
 
     def run(self):
         while self.running:
